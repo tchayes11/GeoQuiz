f2848c80548cf723975c93474b1bbd49d0423892
TinaCH <thayes9@live.spcollege.edu>
2017-03-21 14:14:27 -0400

    Adding additional files

---
+++ app/src/main/java/com/bignerdranch/Question.java
@@ -0,0 +1,32 @@
+package com.bignerdranch;
+
+/**
+ * Created by TinaCH on 3/15/2017.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue){
+        mTextResId= textResId;
+        mAnswerTrue= answerTrue;
+    }
+}
